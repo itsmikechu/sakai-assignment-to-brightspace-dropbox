3488f8aa94f193bbab6025ea2fa841110453d0c8
Mike Chu <mike@chu.ms>
2017-08-04 14:35:05 -0600

    Initial commit w/ app compiling and running.

---
+++ src/app.js
@@ -0,0 +1,12 @@
+require("babel-polyfill");
+
+import fs from 'fs-extra';
+import D2L from 'valence';
+
+class App {
+    static main() {
+        console.log("Application compailes and runs.");
+    }
+}
+
+App.main();
