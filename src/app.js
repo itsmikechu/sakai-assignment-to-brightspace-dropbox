32180d46cd78b4cc961bb74db761a2c248c29385
Mike Chu <mike@chu.ms>
2017-08-04 16:28:16 -0600

    Figuring out how to use the D2L API

--- src/app.js
+++ src/app.js
@@ -3,18 +3,33 @@ require("babel-polyfill");
 import fs from 'fs-extra';
 import D2L from 'valence';
 import config from './config.json';
+import https from 'https';
 
 class App {
     static main() {
-        const userContext = new D2L.UserContext({
-            host : 'courses.ashworthcollege.edu',
-            port : 443,
-            userId : config.userId,
-            userKey : config.userKey,
-            appId : config.appId,
-            appKey : config.appKey,
-        });
-        console.log(userContext.createAuthenticatedUrl('/path', 'GET'));
+        const HOST = 'https://courses.ashworthcollege.edu';
+        const PORT = 443;
+
+        const appContext = new D2L.ApplicationContext(config.appId, config.appKey);
+        const userContext = appContext.createUserContextWithValues(HOST, PORT, config.userId, config.userKey);
+
+        // const urlForAuth = userContext.createAuthenticatedUrl('/d2l/auth/api/token', 'GET');
+        // console.log(urlForAuth);
+        // https.get(urlForAuth, (response) => {
+        //     console.log(response.statusCode);
+        //     response.on('data', (data) => {
+        //         process.stdout.write(data);
+        //     });
+        // });
+
+        // const versionRequestUrl = userContext.createAuthenticatedUrl('/d2l/api/lp/1.2/users/whoami', 'GET');
+        // console.log(versionRequestUrl);
+        // https.get(versionRequestUrl, (response) => {
+        //     console.log(response.statusCode);
+        //     response.on('data', (data) => {
+        //         process.stdout.write(data);
+        //     });
+        // });
     }
 }
 
