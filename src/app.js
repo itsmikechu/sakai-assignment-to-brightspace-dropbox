dbf7beecdbaf37a0ada0d5f688a094e1d7f073e3
Mike Chu <mike@chu.ms>
2017-08-04 15:20:22 -0600

    fixed untracked files

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@ require("babel-polyfill");
 
 import fs from 'fs-extra';
 import D2L from 'valence';
+import config from './config.json';
 
 class App {
     static main() {
-        console.log("Application compailes and runs.");
+        const userContext = new D2L.UserContext({
+            host : 'courses.ashworthcollege.edu',
+            port : 443,
+            userId : config.userId,
+            userKey : config.userKey,
+            appId : config.appId,
+            appKey : config.appKey,
+        });
+        console.log(userContext.createAuthenticatedUrl('/path', 'GET'));
     }
 }
 
